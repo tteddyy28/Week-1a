5773d949141b1417ffb26463f72644155775be5b
tedtessa <ttessa@student.bridgew.edu>
2020-09-08 11:58:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+print("Wlcome to Comp151 ")
+print("Hello Prof")
+age = 5
+# this is a comment
+dog_years = 7 * age
+print(f"The dog is {dog_years}")
+fake_years = "123" # This is not a number
+print(f"fake years is {fake_years}")
+fake_years_2 = fake_years * 7
+print(f"fake years 2 is {fake_years_2}")
